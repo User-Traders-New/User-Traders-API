d58646f7194e532c9baf15b53766f261f8161f2d
jaebin1234 <jaebin2586@gmail.com>
2021-08-26 17:05:20 +0900

    init

---
+++ src/main/java/com/company/usertradersback/UsertradersBackApplication.java
@@ -0,0 +1,13 @@
+package com.company.usertradersback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertradersBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertradersBackApplication.class, args);
+	}
+
+}
