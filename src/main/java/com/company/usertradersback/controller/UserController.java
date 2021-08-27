94a0fb8c0c5e5b0b2af6001ce5a12c637f458e0f
jaebin1234 <jaebin2586@gmail.com>
2021-08-27 13:30:41 +0900

    Usercontroller test

---
+++ src/main/java/com/company/usertradersback/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.company.usertradersback.controller;
+
+public class UserController {
+//
+//    @PostMapping(value = "/signup") // 유저 회원 가입
+//    public ResponseEntity register(@RequestBody UserDto userDto) {
+//        customUserDetailService.save(userDto);
+//        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+//    }
+
+}
